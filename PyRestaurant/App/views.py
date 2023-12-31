c8a9f4d0b6b140a25b80f1813698c5a5287e4917
Francis Aigo <103745801+CoffeeBeansama@users.noreply.github.com>
2023-12-31 21:29:12 +0800

    :+1:

--- PyRestaurant/App/views.py
+++ PyRestaurant/App/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from . import models
+
 
 # Create your views here.
+def greet(request):
+    return HttpResponse(loader.get_template("html/greet.html").render({},request))
